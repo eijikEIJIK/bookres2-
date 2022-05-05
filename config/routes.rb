4e1e24267bc7068fc4a2f748a613ca3e70538103
EC2 Default User <ec2-user@ip-172-31-82-27.ec2.internal>
2022-05-05 07:25:24 +0000

    add PostBook model and add post books controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: "homes#top"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :post_books, only: [:new, :index, :show, :edit]
+
   get '/homes/about', to: 'homes#about', as: 'about'
 end
---
+++ db/migrate/20220505072021_create_post_books.rb
@@ -0,0 +1,11 @@
+class CreatePostBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :post_books do |t|
+      t.string :title
+      t.text :body
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
