2c5caea7a0802dcf55752de5dc64ed7932ecd3da
EC2 Default User <ec2-user@ip-172-31-82-27.ec2.internal>
2022-05-05 15:23:43 +0000

    add top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :users, only: [:index, :show, :edit, :update]
-
   devise_for :users
   root to: "homes#top"
-
+  resources :users, only: [:index, :show, :edit, :update]
   resources :books
 
   get '/homes/about', to: 'homes#about', as: 'about'
