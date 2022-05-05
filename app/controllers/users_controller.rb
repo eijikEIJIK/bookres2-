8bfd4b073930d648edc2bbdfe6d6419ad69bac9d
EC2 Default User <ec2-user@ip-172-31-82-27.ec2.internal>
2022-05-05 14:38:36 +0000

    add side bar (user info and form)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
   def index
+    @book_new = Book.new
     @users=User.all
+    @book=Book.new
   end
-  
+
   def show
+    @book_new = Book.new
     @user = User.find(params[:id])
     @books = @user.books
+    @book=Book.new
   end
 
   def edit
