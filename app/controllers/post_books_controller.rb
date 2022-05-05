305c074a68b0ae8af0d464bf8de5ccd37309aee5
EC2 Default User <ec2-user@ip-172-31-82-27.ec2.internal>
2022-05-05 07:43:31 +0000

    add create action and post page

--- app/controllers/post_books_controller.rb
+++ app/controllers/post_books_controller.rb
@@ -1,8 +1,18 @@
 class PostBooksController < ApplicationController
   def index
+    @post_book = PostBook.new
+    @post_books = PostBook.all
   end
 
   def new
+    @post_book = PostBook.new
+  end
+
+  def create
+    @post_book = PostBook.new(post_book_params)
+    @post_book.user_id = current_user.id
+    @post_book.save
+    redirect_to post_books_path
   end
 
   def show
@@ -10,4 +20,11 @@ class PostBooksController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def post_book_params
+    params.require(:post_book).permit(:title, :body)
+  end
+
 end
