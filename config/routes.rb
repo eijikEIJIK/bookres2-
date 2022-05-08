8dfef7d4c8f351683f6dadaa14056178298be8c9
EC2 Default User <ec2-user@ip-172-31-82-27.ec2.internal>
2022-05-08 08:46:35 +0000

    added favorite func

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create,:destroy]
   end
 
