8ace6c6370f3a7692f94ce7bb3130ca27d2d0ee6
EC2 Default User <ec2-user@ip-172-31-82-27.ec2.internal>
2022-05-08 09:20:24 +0000

    all test passed

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,23 +1,18 @@
 class FavoritesController < ApplicationController
   def create
     book = Book.find(params[:book_id])
+
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    if request.referer&.include?("/books/#{book.id}")
-      redirect_to book_path(book)
-    else
-      redirect_to books_path
-    end
-  end
 
+    redirect_to request.referer
+  end
+  
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    if request.referer&.include?("/books/#{book.id}")
-      redirect_to book_path(book)
-    else
-      redirect_to books_path
-    end
+
+    redirect_to request.referer
   end
 end
